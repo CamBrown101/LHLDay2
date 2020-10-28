4e81e1c7f131790ce1d7a04e91293cb6cba8a8ff
cambrown101 <cameron-brown@live.com>
2020-10-27 18:16:45 -0700

    Day 2 work

--- debuggingErrors/average.js
+++ debuggingErrors/average.js
@@ -1,10 +1,18 @@
-function average(list) {
-  var sum = 0;
+// function average(list) {
+//   var sum = 0;
 
-  for (var num of list) {
-    sum += num;
-  }
+//   for (var num of list) {
+//     sum += num;
+//   }
 
-  return sum / list.length;
+//   return sum / list.length;
+// }
+// console.log(average([3, 5, 7]));
+let cost = 500;
+let type = 'House';
+
+if (cost < 500 && type === 'Apartment') {
+  console.log("I'll buy it!");
+} else {
+  console.log('I won’t buy it!');
 }
-console.log(average([3, 5, 7]));
